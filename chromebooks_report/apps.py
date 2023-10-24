64e44952c2a18268fa9c3117839ee265e76bf040
abrahamprz <fcoabrahamprz@gmail.com>
2023-10-24 14:24:28 -0700

    adds new apps

--- chromebooks_report/apps.py
+++ chromebooks_report/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ChromebookReportConfig(AppConfig):
+class ChromebooksReportConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "chromebook_report"
+    name = "chromebooks_report"
