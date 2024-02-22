f0bd631feef0312d4c234e64f9438bef70fd80a4
Abraham <fcoabrahamprz@gmail.com>
2024-02-22 11:36:33 -0700

    Update school_name field choices in SchoolRecipient model

--- recipients/models.py
+++ recipients/models.py
@@ -8,7 +8,22 @@ class SchoolRecipient(models.Model):
         ("LOST", "Receive lost mode report"),
     ]
 
-    school_name = models.CharField(max_length=255, blank=False)
+    SCHOOLS = [
+        ("ADS", "Arizona Desert Elementary"),
+        ("CCS", "Cesar Chavez Elementary"),
+        ("DVS", "Desert View Elementary"),
+        ("EPS", "Ed Pastor Elementary"),
+        ("GES", "Gadsden Elementary School"),
+        ("RCS", "Rio Colorado Elementary"),
+        ("SLMS", "San Luis Middle School"),
+        ("SLPS", "San Luis Preschool"),
+        ("SWJH", "Southwest Junior High"),
+        ("DAC", "District Administrative Office"),
+        ("ALL", "All sites"),
+        ("TEST", "Test site"),
+    ]
+
+    school_name = models.CharField(max_length=4, choices=SCHOOLS, default="ALL")
     recipient_name = models.CharField(max_length=255, blank=False)
     recipient_email = models.EmailField(blank=False)
     email_type = models.CharField(max_length=7, choices=EMAIL_TYPES, default="GENERAL")
