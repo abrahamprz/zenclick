4011f4fed6b0642fb8b678413c914019e8da42f8
Abraham <fcoabrahamprz@gmail.com>
2024-02-22 08:50:08 -0700

    Refactor tasks.py: Rename create_weekly_report to create_weekly_repair_report and add create_daily_lost_mode_report

--- chromebooks_report/tasks.py
+++ chromebooks_report/tasks.py
@@ -3,8 +3,13 @@ from django.core.management import call_command
 
 
 @shared_task
-def create_weekly_report():
-    call_command("createreport")
+def create_weekly_repair_report():
+    call_command("general")
+
+
+@shared_task
+def create_daily_lost_mode_report():
+    call_command("lostmode")
 
 
 @shared_task
