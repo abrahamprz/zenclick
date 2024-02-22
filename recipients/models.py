d7dfb10f31667126935c853e34bfc98944963b4c
Abraham <fcoabrahamprz@gmail.com>
2024-02-22 08:56:15 -0700

    Add email_type field to SchoolRecipient model

--- recipients/models.py
+++ recipients/models.py
@@ -3,13 +3,19 @@ from django.db import models
 
 # Create your models here.
 class SchoolRecipient(models.Model):
+    EMAIL_TYPES = [
+        ("GENERAL", "General report for principals"),
+        ("LOST", "Lost mode report"),
+    ]
+
     school_name = models.CharField(max_length=255, blank=False)
     recipient_name = models.CharField(max_length=255, blank=False)
     recipient_email = models.EmailField(blank=False)
+    email_type = models.CharField(max_length=6, choices=EMAIL_TYPES, default="GENERAL")
 
     class Meta:
         unique_together = ("school_name", "recipient_name", "recipient_email")
         ordering = ["school_name"]
 
     def __str__(self):
-        return f"{self.school_name} - {self.recipient_name} ({self.recipient_email})"
+        return f"{self.school_name} - {self.recipient_name} ({self.recipient_email}) - {self.get_email_type_display()}"
