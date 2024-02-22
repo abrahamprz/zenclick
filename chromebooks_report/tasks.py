18666b51449e9ad1de01bcd9ff90af1f405dfea3
Abraham <fcoabrahamprz@gmail.com>
2024-02-22 09:45:31 -0700

    Refactor command names in tasks.py

--- chromebooks_report/tasks.py
+++ chromebooks_report/tasks.py
@@ -4,12 +4,12 @@ from django.core.management import call_command
 
 @shared_task
 def create_weekly_repair_report():
-    call_command("general")
+    call_command("generalreport")
 
 
 @shared_task
-def create_daily_lost_mode_report():
-    call_command("lostmode")
+def create_lost_mode_report():
+    call_command("lostmodereport")
 
 
 @shared_task
